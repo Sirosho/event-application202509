2e8a2e31ffc1bafb704efc09d562e4ed3a07061d
Sirosho <pack8447@gmail.com>
2025-09-01 15:39:12 +0900

    [FE] 이벤트 삭제

---
+++ frontend/src/components/EventItem.jsx
@@ -0,0 +1,51 @@
+import styles from './EventItem.module.scss';
+import {useLoaderData, useNavigate} from "react-router-dom";
+
+const EventItem = ({ event }) => {
+    const {
+        title,
+        desc: description,
+        'img-url': image,
+        'start-date': date,
+    } = event;
+
+    const detailEvent = useLoaderData();
+
+    // console.log(detailEvent.id);
+    const navigate = useNavigate();
+
+
+    // delete 요청
+     const deleteEvent = async () => {
+         console.log('삭제요청')
+         const response = await fetch(`http://localhost:9000/api/events/${detailEvent.id}`, {
+             method: 'DELETE'
+         })
+         if(response.ok) {
+             alert('삭제되었습니다.');
+             console.log(response.json())
+             // 이벤트 페이지로 돌아가기
+             navigate('/events');
+         }
+     }
+
+
+
+    return (
+        <article className={styles.event}>
+            <img
+                src={image}
+                alt={title}
+            />
+            <h1>{title}</h1>
+            <time>{date}</time>
+            <p>{description}</p>
+            <menu className={styles.actions}>
+                <a href='#'>Edit</a>
+                <button onClick={deleteEvent}>Delete</button>
+            </menu>
+        </article>
+    );
+};
+
+export default EventItem;
