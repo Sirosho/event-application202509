ca21684ab09aa3289da147c2f6f14b33d490c76f
Sirosho <pack8447@gmail.com>
2025-09-01 12:26:17 +0900

    [FE] loader로 서버데이터 페칭하기

--- frontend/src/pages/EventPage.jsx
+++ frontend/src/pages/EventPage.jsx
@@ -1,24 +1,29 @@
-import React, {useEffect, useState} from 'react';
-import {Link} from 'react-router-dom';
+import React from 'react';
+import {useLoaderData} from 'react-router-dom';
 import EventList from '../components/EventList.jsx';
 
 const EventPage = () => {
 
-    const [eventList, setEventList] = useState([]);
+    // loader가 리턴한 데이터 가져오기
+    const eventList = useLoaderData();
 
-    // API CALL
-    const fetchEvents = async () => {
-        const response = await fetch('http://localhost:9000/api/events');
-        const data = await response.json();
-        console.log(data);
+    // const [eventList, setEventList] = useState([]);
 
-        setEventList(data);
-    };
+    // // API CALL
+    // const fetchEvents = async () => {
+    //     const response = await fetch('http://localhost:9000/api/events');
+    //     const data = await response.json();
+    //     console.log(data);
+    //
+    //     setEventList(data);
+    // };
 
 
-    useEffect(() => {
-        fetchEvents();
-    }, []);
+    // useEffect(() => {
+    //     console.log('서버에서 데이터 페칭!');
+    //     fetchEvents();
+    // }, []);
+    //
 
     return (
         <EventList eventList={eventList} />
