e2d90f70210e79d7940989d2366d651147826836
Sirosho <pack8447@gmail.com>
2025-09-01 14:09:06 +0900

    [FE] 이벤트 생성 페이지

--- frontend/src/loader/events-loader.js
+++ frontend/src/loader/events-loader.js
@@ -6,6 +6,7 @@ export const eventListLoader = async() => {
     // loader가 리턴한 데이터는 라우팅 된 페이지와
     // 그 하위 컴포넌트에서 언제든 사용가능
     // loader는 fetch결과를 바로 리턴하는 경우 알아서 json을 추출한다.
+    // 한번 렌더링 하면 되는 페이지는 로더로 처리함
     return response;
 }
 
