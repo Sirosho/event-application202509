b944bfa817f77d3cf037d6bf871317d2b6f29463
Sirosho <pack8447@gmail.com>
2025-09-01 11:12:06 +0900

    [BE] 전역 크로스오리진 설정

--- frontend/src/pages/EventPage.jsx
+++ frontend/src/pages/EventPage.jsx
@@ -1,12 +1,25 @@
-import React from 'react';
+import React, {useState} from 'react';
 import MainNavigation from "../components/MainNavigation.jsx";
 
 const EventPage = () => {
+
+    const [eventList, setEventList] = useState([]);
+
+    // API CALL
+    const fetchEvents = async () => {
+        const response =
+            await fetch('http://localhost:9000/api/events');
+            const data = await response.json();
+        console.log(data);
+    };
+    fetchEvents();
+
     return (
         <>
-        <h1>
-           Event Page
-        </h1>
+        <h1>Event Page</h1>
+            <ul>
+                {eventList.map((event) => <li>{event}</li>)}
+            </ul>
 
         </>
     );
--- frontend/src/routes/route-config.jsx
+++ frontend/src/routes/route-config.jsx
@@ -8,17 +8,16 @@ import RootLayout from "../layouts/RootLayout.jsx";
 const router = createBrowserRouter([
     {
         path: "/", // 이 경로와 칠드런의 경로를 조합해서 씀
-        element: <RootLayout />,
-        errorElement: <ErrorPage />,
+        element: <RootLayout/>,
+        errorElement: <ErrorPage/>,
         children: [
             {
                 index: true, // path가 비어있을때 사용
-                element: <HomePage />
+                element: <HomePage/>
             },
             {
                 path: "events",
-                element: <EventPage />
-
+                element: <EventPage/>
             }
         ]
     },
