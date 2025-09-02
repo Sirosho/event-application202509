579e3bc80546192a4a61c9fdd37f6a6ba0d1d7c6
Sirosho <pack8447@gmail.com>
2025-09-02 11:23:41 +0900

    [FE] 전체조회 프론트엔드 useEffect 사용 무한스크롤로 변경

--- frontend/src/pages/EventPage.jsx
+++ frontend/src/pages/EventPage.jsx
@@ -1,32 +1,28 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {useLoaderData} from 'react-router-dom';
 import EventList from '../components/EventList.jsx';
 
 const EventPage = () => {
 
     // loader가 리턴한 데이터 가져오기
-    const eventList = useLoaderData();
+    // const {hasNext , eventList} = useLoaderData();
 
-    // const [eventList, setEventList] = useState([]);
+    const [eventList, setEventList] = useState([]);
 
-    // // API CALL
-    // const fetchEvents = async () => {
-    //     const response = await fetch('http://localhost:9000/api/events');
-    //     const data = await response.json();
-    //     console.log(data);
-    //
-    //     setEventList(data);
-    // };
+    useEffect(() => {
 
 
-    // useEffect(() => {
-    //     console.log('서버에서 데이터 페칭!');
-    //     fetchEvents();
-    // }, []);
-    //
+        (async () => {
+            const response = await fetch('http://localhost:9000/api/events?page=1');
+            const {hasNext, eventList: events} = await response.json();
+            setEventList(events)
+        })();
+
+    },[]);
+
 
     return (
-        <EventList eventList={eventList} />
+        <EventList eventList={eventList}/>
     );
 };
 
