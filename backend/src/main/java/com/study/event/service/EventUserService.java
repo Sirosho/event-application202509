d726c94b190ca31d63315f553e9e73352f2a0e5d
Sirosho <pack8447@gmail.com>
2025-09-03 09:29:39 +0900

    [BE] 이메일 중복확인

---
+++ backend/src/main/java/com/study/event/service/EventUserService.java
@@ -0,0 +1,32 @@
+package com.study.event.service;
+
+import com.study.event.domain.entity.EventUser;
+import com.study.event.repository.EventUserRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+@Transactional
+@Slf4j
+public class EventUserService {
+
+    private final EventUserRepository eventUserRepository;
+
+    // 이메일 중복확인 처리
+    public boolean checkEmailDuplicate(String email){
+        // 임시로 회원가입 진행
+        EventUser eventUser = EventUser.builder()
+                .email("abc@def.com" + (int) (Math.random() * 99999))
+                .build();
+
+        eventUserRepository.save(eventUser);
+        // 중복확인
+        boolean flag = eventUserRepository.existsByEmail(email);
+        log.info("email {} is duplicate  : {}",email, flag);
+        return flag;
+    }
+
+}
