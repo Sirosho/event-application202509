a40a24618cae0ae40ae8667e0bbf8a843d570860
Sirosho <pack8447@gmail.com>
2025-09-04 12:18:12 +0900

    [FE] loader로 라우트가드 하기

--- frontend/src/loader/events-loader.js
+++ frontend/src/loader/events-loader.js
@@ -1,17 +1,40 @@
+import {EVENT_API_URL} from '../config/host-config.js';
+import {redirect} from 'react-router-dom';
 
-import {EVENT_API_URL } from '../config/host-config.js';
-export const eventListLoader = async() => {
+export const eventListLoader = async () => {
+    const response = await fetch(`${EVENT_API_URL}?page=1`);
 
-    const response = await fetch(`${EVENT_API_URL}?page=76`);
-
-    // loader가 리턴한 데이터는 라우팅 된 페이지와
-    // 그 하위 컴포넌트에서 언제든 사용가능
+    // loader가 리턴한 데이터는 라우팅된 페이지와 그 하위 컴포넌트에서 언제든 사용가능
     // loader는 fetch결과를 바로 리턴하는 경우 알아서 json을 추출한다.
-    // 한번 렌더링 하면 되는 페이지는 로더로 처리함
+    // return await response.json();
     return response;
 }
 
-export const eventDetailLoader= async({params}) => {
+export const eventDetailLoader = async ({ params }) => {
 
     return await fetch(`${EVENT_API_URL}/${params.eventId}`);
 }
+
+// 토큰데이터를 파싱하는 함수
+const parseUserData = () => JSON.parse(localStorage.getItem('userData'));
+
+// 로컬스토리지에 있는 토큰데이터를 불러오는 로더
+export const userDataLoader = () => parseUserData();
+
+const isLoggedIn = () => parseUserData() !== null;
+
+// 로그인 여부를 검사하여 로그인하지 않았다면 로그인페이지로 돌려보내는 로더
+export const authCheckLoader = () => {
+    if (!isLoggedIn()) {
+        alert('로그인이 필요한 서비스입니다.');
+        return redirect('/');
+    }
+    return null; // 현재 페이지에 머물게 됨
+};
+
+
+
+
+
+
+
--- frontend/src/routes/route-config.jsx
+++ frontend/src/routes/route-config.jsx
@@ -4,37 +4,36 @@ import EventPage from '../pages/EventPage.jsx';
 import RootLayout from '../layouts/RootLayout.jsx';
 import EventDetailPage from '../pages/EventDetailPage.jsx';
 import EventLayout from '../layouts/EventLayout.jsx';
-import {eventDetailLoader} from '../loader/events-loader.js';
+import {authCheckLoader, eventDetailLoader, userDataLoader} from '../loader/events-loader.js';
 import {deleteAction, saveAction as manipulateAction, loginAction, logoutAction} from '../loader/events-actions.js';
 import NewEventPage from '../pages/NewEventPage.jsx';
 import EditPage from '../pages/EditPage.jsx';
-import HomeLayout from "../layouts/HomeLayout.jsx";
-import WelcomePage from "../pages/WelcomePage.jsx";
-import SignUpPage from "../pages/SignUpPage.jsx";
+import HomeLayout from '../layouts/HomeLayout.jsx';
+import WelcomePage from '../pages/WelcomePage.jsx';
+import SignUpPage from '../pages/SignUpPage.jsx';
 
 const router = createBrowserRouter([
     {
         path: '/',
         element: <RootLayout/>,
         errorElement: <ErrorPage/>,
+        loader: userDataLoader,
+        // loader의 리턴데이터는 children(Outlet)들에게는 전달되지 않는게 기본
+        // 그런데 id를 주면 Children들이 id로 가져갈 수 있음
+        id: 'user-token-data',
         children: [
             {
                 path: '',
-                element: <HomeLayout/>,
+                element: <HomeLayout />,
                 children: [
                     {
                         index: true,
-                        element: <WelcomePage/>,
+                        element: <WelcomePage />,
                         action: loginAction,
-                        loader:()=>{
-                            const userData = JSON.parse(localStorage.getItem('userData'));
-
-                            return userData;
-                        }
                     },
                     {
-                        path: 'sign-up',
-                        element: <SignUpPage/>,
+                        path: '/sign-up',
+                        element: <SignUpPage />
                     },
                     {
                         path: '/logout',
@@ -45,12 +44,13 @@ const router = createBrowserRouter([
             {
                 path: 'events',
                 element: <EventLayout/>,
+                loader: authCheckLoader, // 라우트 가드
                 children: [
                     {
                         index: true,
                         element: <EventPage/>,
                         // loader함수는 언제 실행되냐? 페이지가 라우팅될 때 트리거됨
-
+                        // loader: eventListLoader
                     },
                     {
                         path: 'new',
